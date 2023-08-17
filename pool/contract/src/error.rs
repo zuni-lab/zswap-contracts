e31164aaa39ad2ea3d7e67c8eea1e372d5b601d8
tranvinh146 <tranvinhlx135@gmail.com>
2023-08-17 09:15:35 +0700

    add error

---
+++ pool/contract/src/error.rs
@@ -0,0 +1,6 @@
+pub const ALREADY_INITIALIZED: &str = "Already initialized";
+pub const INSUFFICIENT_INPUT_AMOUNT: &str = "Insufficient input amount";
+pub const INVALID_PRICE_LIMIT: &str = "Invalid price limit";
+pub const INVALID_TICK_RANGE: &str = "Invalid tick range";
+pub const NOT_ENOUGH_LIQUIDITY: &str = "Not enough liquidity";
+pub const ZERO_LIQUIDITY: &str = "Zero liquidity";
